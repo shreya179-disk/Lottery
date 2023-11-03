3d92200855cabaea06e31c1f28e6b268ace0ae5e
shreya <shreya@shreyas-MacBook-Air-2.local>
2023-11-04 02:03:31 +0530

    first commit

---
+++ src/lotter.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract lottery{
+    address public manager;
+    address[] public Players;
+
+    constructor () {
+        manager = msg.sender;
+    }
+
+    function Enter(address new_player) public payable {
+        require(msg.value > 0.01 ether,"didnt send one ether");
+        Players.push(new_player);
+    }
+    
+    function random() private view returns(uint){
+      return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, Players)));
+    
+    }
+
+    function pickWinner() public {
+       uint index= random()% Players.length;
+        payable(address(uint160(Players[index]))).transfer(address(this).balance);
+    }
+}
